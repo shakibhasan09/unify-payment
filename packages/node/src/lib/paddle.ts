388d87d0de48814fb53419e78a45dcda2df6cd86
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-08-24 18:24:14 +0600

    feat: added paddle verifySignature

--- packages/node/src/lib/paddle.ts
+++ packages/node/src/lib/paddle.ts
@@ -12,4 +12,36 @@ export class UnifyPaddle {
 
     return transaction.checkout.url;
   }
+
+  webhook = new UnifyPaddleWebhook(this.paddle);
+}
+
+export class UnifyPaddleWebhook {
+  constructor(private paddle: Paddle) {}
+
+  async verifySignature(payload: {
+    signature: string;
+    secret: string;
+    body: string;
+  }) {
+    try {
+      const event = this.paddle.webhooks.unmarshal(
+        payload.body,
+        payload.secret,
+        payload.signature
+      );
+
+      if (!event) {
+        throw new Error("Event verification failed");
+      }
+
+      return {
+        event,
+      };
+    } catch (err) {
+      return {
+        error: err as Error,
+      };
+    }
+  }
 }
