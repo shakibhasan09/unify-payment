11eb7d3c872da5062bb87e64d55e60cb76e8b087
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-08-26 19:19:13 +0600

    added

--- packages/node/src/lib/common.ts
+++ packages/node/src/lib/common.ts
@@ -5,6 +5,7 @@ import { UnifyPaddle } from "./paddle";
 import { SSLCommerz, UnifySSLCommerz } from "./sslcommerz";
 import { UnifyStripe } from "./stripe";
 import { Paypal, UnifyPaypal } from "./paypal";
+import { Bkash, UnifyBkash } from "./bkash";
 
 export type UnifyPaymentOptions = {
   stripe?: Stripe;
@@ -12,6 +13,7 @@ export type UnifyPaymentOptions = {
   sslcommerz?: SSLCommerz;
   paddle?: Paddle;
   paypal?: Paypal;
+  bkash?: Bkash;
 };
 
 export class UnifyPayment<T extends UnifyPaymentOptions = UnifyPaymentOptions> {
@@ -22,6 +24,8 @@ export class UnifyPayment<T extends UnifyPaymentOptions = UnifyPaymentOptions> {
     : undefined;
   paddle: T["paddle"] extends Paddle ? UnifyPaddle : undefined;
   paypal: T["paypal"] extends Paypal ? UnifyPaypal : undefined;
+
+  bkash: T["bkash"] extends Bkash ? UnifyBkash : undefined;
 
   constructor(options: T) {
     // stripe
@@ -42,6 +46,10 @@ export class UnifyPayment<T extends UnifyPaymentOptions = UnifyPaymentOptions> {
     // paddle
     this.paddle = new UnifyPaddle(options.paddle!) as T["paddle"] extends Paddle
       ? UnifyPaddle
+      : undefined;
+
+    this.bkash = new UnifyBkash(options.bkash!) as T["bkash"] extends Bkash
+      ? UnifyBkash
       : undefined;
 
     // paypal
