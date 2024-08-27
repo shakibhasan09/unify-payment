05ba1083623a4236bce300dd3249ce4aa21a5533
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-08-28 00:33:05 +0600

    removed paddle

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,6 +1,5 @@
 import { Bkash } from "./lib/bkash";
 import { LemonSqueezy } from "./lib/lemonsqueezy";
-import { Paddle } from "./lib/paddle";
 import { Paypal } from "./lib/paypal";
 import { SSLCommerz } from "./lib/sslcommerz";
 import { Stripe } from "./lib/stripe";
@@ -8,7 +7,6 @@ import { Stripe } from "./lib/stripe";
 const UnifyPayment = {
   LemonSqueezy,
   Bkash,
-  Paddle,
   Paypal,
   SSLCommerz,
   Stripe,
